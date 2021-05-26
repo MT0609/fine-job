25c5039b38f08a1770e5d782e3a42210d394ea8e
NhatTai20 <ntai20111@gmail.com>
2021-05-26 08:39:21 +0700

    edit job service

--- client/src/actions/jobActions.js
+++ client/src/actions/jobActions.js
@@ -19,7 +19,7 @@ export const getJobs =
       // if (page) params.page = +page;
 
       let result = await jobApi.get(title, page, limit);
-
+      console.log(result);
       dispatch({
         type: JOBCONSTANTS.JOB_GET_ALL_SUCCESS,
         payload: result,
