3f67009dfe4ffd38fced3d8e32942ac10012fd6b
Nguyen Quoc Thai <ct1.nquocthai@gmail.com>
2021-06-28 11:09:05 +0700

    fix socket delay

--- server/src/socket.js
+++ server/src/socket.js
@@ -28,19 +28,24 @@ module.exports.socket = (server) => {
         }
       });
       socketsConnected.add({ skId: sk.id, userId });
+      console.log(socketsConnected);
     });
 
     try {
       // 1 vs 1 message
       sk.on('new-1-1-msg', ({ receiver, data }) => {
         console.log('New message from: ', sk.id);
+        let sen = null;
         let rev = null;
         socketsConnected.forEach((conn) => {
+          if (conn.skId == sk.id) {
+            sen = conn;
+          }
           if (conn.userId == receiver) {
             rev = conn;
           }
         });
-        sk.to(rev?.skId || '').emit('server-res-1-1-msg', sk.id);
+        sk.to(rev?.skId || '').emit('server-res-1-1-msg', sen?.userId || '');
       });
     } catch (error) {
       console.log({ error });
